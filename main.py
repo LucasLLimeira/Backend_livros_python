1e74263b482c8cd6778c4a69918176456c65e047
Lucas De Lucena Limeira <lucasdllimeira@hotmail.com>
2026-05-11 16:37:25 -0300

    refactor: update livro model and simplify criar_livro and atualizar_livro functions

--- main.py
+++ main.py
@@ -8,10 +8,17 @@
 #Delete - Deletar dados
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 meus_livros = {}
+
+class Livro(BaseModel):
+    titulo: str
+    autor: str
+    lancamento: int
 
 @app.get("/livros")
 def ler_livros():
@@ -20,24 +27,19 @@ def ler_livros():
     return meus_livros
 
 @app.post("/adicionar_livros")
-def criar_livro(id: int, titulo: str, autor: str, lancamento: int):
+def criar_livro(id: int, livro: Livro):
     if id in meus_livros:
         raise HTTPException(status_code=400, detail="Livro já existe")
     else:
-        meus_livros[id] = {"titulo": titulo, "autor": autor, "lancamento": lancamento}
+        meus_livros[id] = livro.model_dump()
     return {"detail": "Livro adicionado com sucesso", "livro": meus_livros[id]}
 
 @app.put("/atualizar_livros/{id}")
-def atualizar_livro(id: int, titulo: str = None, autor: str = None, lancamento: int = None):
+def atualizar_livro(id: int, livro: Livro):
     if id not in meus_livros:
         raise HTTPException(status_code=404, detail="Livro não encontrado")
     else:
-        if titulo:
-            meus_livros[id]["titulo"] = titulo
-        if autor:
-            meus_livros[id]["autor"] = autor
-        if lancamento:
-            meus_livros[id]["lancamento"] = lancamento
+        meus_livros[id] = livro.model_dump()
     return {"detail": "Livro atualizado com sucesso", "livro": meus_livros[id]}
 
 @app.delete("/deletar_livros/{id}")
