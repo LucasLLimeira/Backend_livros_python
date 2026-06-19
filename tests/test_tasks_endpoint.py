9d4b55b3a54a9152beb857ebbc79ef36ab367197
Lucas De Lucena Limeira <lucasdllimeira@hotmail.com>
2026-06-19 14:57:37 -0300

    feat: add test for calcular_fatorial endpoint in test_tasks_endpoint.py

--- tests/test_tasks_endpoint.py
+++ tests/test_tasks_endpoint.py
@@ -25,4 +25,23 @@ def test_calcular_soma(mocker):
     mock_redis_ltrim.assert_called_once()
 
 
+def test_calcular_fatorial(mocker):
+    mock_fatorial_delay = mocker.patch("tasks.fatorial.delay")
+    mock_redis_setex = mocker.patch("main.redis_client.setex")
+    mock_redis_lpush = mocker.patch("main.redis_client.lpush")
+    mock_redis_ltrim = mocker.patch("main.redis_client.ltrim")
 
+    mock_fatorial_delay.return_value.id = "test-task-id"
+    mock_redis_setex.return_value = True
+
+    response = client.post("/calcular/fatorial", params={"n": 5})
+    assert response.status_code == 200
+    assert response.json() == {
+        "task_id": "test-task-id",
+        "tipo": "fatorial",
+        "entrada": {"n": 5},
+        "status": "Pendente",
+        "message": "Tarefa de fatorial iniciada. Use a lista de tarefas para acompanhar o andamento."}
+    mock_redis_setex.assert_called_once()
+    mock_redis_lpush.assert_called_once()
+    mock_redis_ltrim.assert_called_once()
