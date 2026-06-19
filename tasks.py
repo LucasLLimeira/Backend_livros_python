ebd890dbb6ec6e64001a3938a8426c76cb76b884
Lucas De Lucena Limeira <lucasdllimeira@hotmail.com>
2026-06-19 14:54:04 -0300

    feat: update poetry.lock and pyproject.toml for dev dependencies; add tests for tasks

--- tasks.py
+++ tasks.py
@@ -1,15 +1,13 @@
 from celery_app import celery_app
-import time
+
 
 @celery_app.task(name="tasks.somar", bind=True)
-def somar(self, a, b):
-    time.sleep(5)  # Simula uma tarefa demorada
+def somar(self, a, b):  # Simula uma tarefa demorada
     
     return a + b
 
 @celery_app.task(name="tasks.fatorial", bind=True)
-def fatorial(self, n):
-    time.sleep(5)  # Simula uma tarefa demorada
+def fatorial(self, n):  # Simula uma tarefa demorada
     if n < 0:
         raise ValueError("Fatorial não é definido para números negativos")
     elif n == 0 or n == 1:
---
+++ tests/test_tasks_endpoint.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_calcular_soma(mocker):
+    mock_somar_delay = mocker.patch("tasks.somar.delay")
+    mock_redis_setex = mocker.patch("main.redis_client.setex")
+    mock_redis_lpush = mocker.patch("main.redis_client.lpush")
+    mock_redis_ltrim = mocker.patch("main.redis_client.ltrim")
+
+    mock_somar_delay.return_value.id = "test-task-id"
+    mock_redis_setex.return_value = True
+
+    response = client.post("/calcular/soma", params={"a": 3, "b": 4})
+    assert response.status_code == 200
+    assert response.json() == {
+        "task_id": "test-task-id",
+        "tipo": "soma",
+        "entrada": {"a": 3, "b": 4},
+        "status": "Pendente",
+        "message": "Tarefa de soma iniciada. Use a lista de tarefas para acompanhar o andamento."}
+    mock_redis_setex.assert_called_once()
+    mock_redis_lpush.assert_called_once()
+    mock_redis_ltrim.assert_called_once()
+
+
+
